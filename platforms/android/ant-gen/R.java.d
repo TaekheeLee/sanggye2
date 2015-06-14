e7e90e8c4a640800bd9fa526920d8fc2aadc5aa4
TaekheeLee <thlee@suholdings.com>
2015-06-14 18:38:06 +0900

    and remove

---
+++ platforms/android/ant-gen/R.java.d
@@ -0,0 +1,9 @@
+D:\PhoneGap\sanggye2\platforms\android\ant-gen\io\cordova\hellocordova\R.java \
+ : D:\PhoneGap\sanggye2\platforms\android\res\drawable\icon.png \
+D:\PhoneGap\sanggye2\platforms\android\res\drawable-hdpi\icon.png \
+D:\PhoneGap\sanggye2\platforms\android\res\drawable-ldpi\icon.png \
+D:\PhoneGap\sanggye2\platforms\android\res\drawable-mdpi\icon.png \
+D:\PhoneGap\sanggye2\platforms\android\res\drawable-xhdpi\icon.png \
+D:\PhoneGap\sanggye2\platforms\android\res\values\strings.xml \
+D:\PhoneGap\sanggye2\platforms\android\res\xml\config.xml \
+D:\PhoneGap\sanggye2\platforms\android\ant-build\AndroidManifest.xml \
